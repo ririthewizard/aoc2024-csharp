7dec903ed81b7daa931dde8f197327a96eb0bace
ririthewizard <ririwilliams.rw@gmail.com>
2025-06-21 00:35:12 -0500

    Righteous! Day 1 part 1 complete. Time for bed

--- day1/DayOne/Utilities.cs
+++ day1/DayOne/Utilities.cs
@@ -2,11 +2,11 @@ public class Utilities
 {
     private static string fileName = "numbers.txt";
 
-    internal static List<List<int>> LoadLists(List<int> numbers)
+    internal static int LoadLists(List<int> numbers)
     {
-        List<List<int>> loadedLists = new List<List<int>>();
         try
         {
+            int totalDistances = 0;
             if (File.Exists(fileName))
             {
                 numbers.Clear();
@@ -14,19 +14,9 @@ public class Utilities
                 List<int> firstList = new List<int>();
                 List<int> secondList = new List<int>();
 
-                //var numbersList = File.ReadAllLines(fileName)
-                //.Select(line => line.Split(""));
                 char[] delimiterChars = { ' ', '\n', '\t', '\r' };
                 var numbersSplit = File.ReadAllLines(fileName).Select(line => line.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries)).ToList();
 
-                /*foreach (var nums in numbersSpaceSplit)
-                {
-                    foreach (var num in nums)
-                    {
-                        Console.WriteLine(num);
-                    }
-                }
-                */
 
                 foreach (var nums in numbersSplit)
                 {
@@ -43,25 +33,43 @@ public class Utilities
                     }
                 }
 
+                List<int> firstListSorted = firstList.OrderBy(n => n).ToList();
+
+                List<int> secondListSorted = secondList.OrderBy(n => n).ToList();
 
 
+                for (int i = 0; i < firstListSorted.Count; i++)
+                {
+                    int first = firstListSorted[i];
+                    int second = secondListSorted[i];
+                    if (first > second)
+                    {
+                        totalDistances += (first - second);
+                    }
+                    else
+                    {
+                        totalDistances += (second - first);
+                    }
+                }
 
+                Console.WriteLine(totalDistances);
             }
-            return loadedLists;
+            return totalDistances;
         }
         catch (IndexOutOfRangeException eor)
         {
             Console.ForegroundColor = ConsoleColor.DarkRed;
             Console.WriteLine("Tried to select an index that doesn't exist");
             Console.WriteLine(eor);
-            return loadedLists;
+            return 0;
         }
         catch (Exception e)
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Something went wrong!");
             Console.WriteLine(e);
-            return loadedLists;
+            return 0;
         }
     }
+
 }
