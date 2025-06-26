831c2919308c726f9e531daee0644694845bfcbb
ririthewizard <ririwilliams.rw@gmail.com>
2025-06-25 23:57:33 -0500

    Stopping for the night. Need to work out how to convert string[] to my List<List<int>> data type

---
+++ DayTwo/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode2024.Day2;
+
+public class Solution
+{
+    internal static List<List<int>> Extract(string file)
+    {
+        List<List<int>> records = new List<List<int>>();
+        if (File.Exists(file))
+        {
+            var splitLines = File.ReadAllLines(file).Select(line => line.Split("\n"));
+
+            foreach (var line in splitLines)
+            {
+
+            }
+        }
+
+        return records;
+    }
+}
