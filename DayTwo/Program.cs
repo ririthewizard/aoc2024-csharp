6a60f6dfb11633e6e7cc39d85eaae3de57bab119
ririthewizard <ririwilliams.rw@gmail.com>
2025-06-26 18:53:21 -0500

    Getting closer. record is currently keeping the same values even though chars is changing

--- DayTwo/Program.cs
+++ DayTwo/Program.cs
@@ -7,14 +7,35 @@ public class Solution
         List<List<int>> records = new List<List<int>>();
         if (File.Exists(file))
         {
-            var splitLines = File.ReadAllLines(file).Select(line => line.Split("\n"));
+            var splitLines = File.ReadLines(file).Select(line => line.Split("\n"));
 
             foreach (var line in splitLines)
             {
-
+                Console.WriteLine(line);
+                List<int> record = new List<int>();
+                for (int i = 0; i < line.Length; i++)
+                {
+                    var chars = line[i].Split(" ");
+                    // foreach (var c in chars)
+                    // {
+                    //     record.Add(Int32.Parse(c));
+                    // }
+                    Console.WriteLine(record);
+                    records.Add(record);
+                }
+                record.TrimExcess();
+                record.Clear();
             }
         }
 
         return records;
     }
+
+
+    public static void Main(string[] args)
+    {
+        string filePath = "/home/void/workspace/github.com/ririthewizard/aoc2024-csharp/DayTwo/reports.txt";
+        Extract(filePath);
+    }
 }
+
